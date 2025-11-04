830c6dab61ef207df0cb8c4170244920c273b6df
Anthony Higareda <higaredajra@msoe.edu>
2025-11-04 16:09:45 -0600

    Complete signal_alarm

--- signal_alarm.c
+++ signal_alarm.c
@@ -5,11 +5,33 @@
  * Brief summary of program:
  */
 
- #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void handle_alarm()
+{
+    printf("Alarm received!\n");
+    exit(0);
+}
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello world\n");
+
+    signal(SIGALRM, handle_alarm);
+
+    int seconds = 5;
+
+    alarm(seconds);
+
+    printf("Waiting for an alarm...\n");
+    while (1)
+    {
+        printf("Waiting.......\n");
+        
+    }
+    
     return 0;
 }
  
