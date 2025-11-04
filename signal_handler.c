07c98a50068643e5c14ff2873b051449e5d9895f
Anthony Higareda <higaredajra@msoe.edu>
2025-11-04 15:51:22 -0600

    Complete part 2: signal handler

--- signal_handler.c
+++ signal_handler.c
@@ -4,9 +4,14 @@
  */
 
 /**
- * Modified by:
+ * Modified by: Anthony Higareda
  * 
- * Brief summary of modifications:
+ * Brief summary of modifications: Added an integer variable that is incremented every second
+ * Every 5 seconds, we will send a SIGINT
+ * To completely exit this program, it must first be suspended (Ctrl + Z).
+ * Next issue ~$ps and find the pid of this program
+ * Finally, issue ~$kill -SIGKILL <pid>
+ *  replacing <pid> with the pid of this program.
  */
 
 
@@ -20,7 +25,7 @@
  */
 void handle_signal() {
     printf("Received a signal\n");
-    exit(1);
+    // exit(1);
 }
 
 int main() {
@@ -28,9 +33,16 @@ int main() {
     // Register for the signal
     signal(SIGINT, handle_signal);
 
+    int killer = 1; // Control variable for intermittent signal sending
+
     // Wait until a signal is received
     while(1) {
         printf("Sleeping\n");
+        if (killer % 5 == 0) // Every 5 seconds we will send a signal
+        {
+            kill(getpid(), SIGINT);
+        }
+        killer++;
         sleep(1);
     }
 
