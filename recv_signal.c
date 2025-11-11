c1a14624729d5ce3fe4441285c1f2c857118eb2c
Anthony Higareda <higaredajra@msoe.edu>
2025-11-11 15:24:30 -0600

    Complete part 4

--- recv_signal.c
+++ recv_signal.c
@@ -1,6 +1,37 @@
 /**
- * File: 
- * Modified by:
+ * File: recv_signal.c
+ * Modified by: Anthony Higareda
  * 
- * Brief summary of program:
+ * Brief summary of program: Register a sigaction to handle a SIGUSR1 signal.
+ * The received signal will have a sival_int value that will be printed
+ * upon receiving a signal from send_signal.c
  */
+
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void handle_sigusr1(int signo, siginfo_t *info, void *context)
+{
+    printf("Signal received: %d\n", info -> si_value.sival_int);
+}
+
+int main(int argc, char const *argv[])
+{
+    struct sigaction receiver;
+    
+
+    
+    receiver.sa_sigaction = handle_sigusr1;
+    receiver.sa_flags = SA_SIGINFO;
+    sigemptyset(&receiver.sa_mask);
+
+    sigaction(SIGUSR1, &receiver, NULL);
+
+    while (1) 
+    {
+        pause();
+    }
+    return 0;
+}
--- send_signal.c
+++ send_signal.c
@@ -1,6 +1,30 @@
 /**
- * File: 
- * Modified by:
+ * File: send_signal.c
+ * Modified by: Anthony Higareda
  * 
- * Brief summary of program:
+ * Brief summary of program: Using sigqueue, send a SIGUSR1 to recv_signal with
+ * and integer stored to sival_int. Gets the PID of recv_signal from command line upon
+ * invoking the program
  */
+
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char const *argv[])
+{
+
+    pid_t recv_pid;
+    recv_pid = atoi(argv[1]);
+    srand(time(NULL));
+    int data = rand() % 100;
+
+    union sigval value;
+    value.sival_int = data;
+
+    sigqueue(recv_pid, SIGUSR1, value);
+    printf("Random number sent: %d\n", value.sival_int);
+    return 0;
+}
